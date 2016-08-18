1be13b4e97c5509976c534abf9de83766a8677b7
Rory O'Connell <rory.oconnell@grandrounds.com>
2016-08-18 12:01:25 -0700

    More documentation, fixing style

--- lib/grnds/ediot/parser.rb
+++ lib/grnds/ediot/parser.rb
@@ -5,30 +5,32 @@ module Grnds
       include SegmentParser
 
       DEFINITION = {
-        :INS => {size: 17 },
-        :REF => {occurs: 5, size: 2 },
-        :DTP => {occurs: 3, size: 3 },
-        :NM1 => {occurs: 2, size: 9 },
-        :PER => {size: 8 },
-        :N3 => {size: 2 },
-        :N4 => {size: 3 },
-        :DMG => {size: 3 },
-        :HLH => {size: 3 },
-        :HD => {size: 5 },
-        :AMT => {size: 2 }
+        INS: {size: 17 },
+        REF: {occurs: 5, size: 2 },
+        DTP: {occurs: 3, size: 3 },
+        NM1: {occurs: 2, size: 9 },
+        PER: {size: 8 },
+        N3: {size: 2 },
+        N4: {size: 3 },
+        DMG: {size: 3 },
+        HLH: {size: 3 },
+        HD: {size: 5 },
+        AMT: {size: 2 }
       }
 
-      attr_reader :segment_header
-
+      # @param definition [Hash{Symbol => Hash{Symbol => Number}}]
       def initialize(definition=DEFINITION)
         @record = Record.new(definition)
-        @segment_keys = definition.keys.map{|k| k.to_s }
+        @segment_keys = definition.keys.map{ |k| k.to_s }
       end
 
+      # @return [Array<String>]
       def row_keys
         @record.row_keys
       end
 
+      # @param io_in [IO]
+      # @return [Array<Array<String>>]
       def parse(io_in, &block)
         record_lines = []
         collecting = false
@@ -48,6 +50,8 @@ module Grnds
         process_record(record_lines, &block)
       end
 
+      # @param file_lines [String]
+      # @return [Array<Array<String>>]
       def file_parse(file_lines)
         record_rows = []
         parse(StringIO.new(file_lines)) do |row|
@@ -56,22 +60,28 @@ module Grnds
         record_rows
       end
 
-      def process_record(record_lines, &block)
+      private def process_record(record_lines, &block)
         unless record_lines.empty?
           row = @record.parse(record_lines)
           block.call(row)
         end
       end
 
+      # @param line [String]
+      # @return [Bool]
       def is_known_line_type?(line)
         line_key = segment_peek(line)
         @segment_keys.include?(line_key)
       end
 
+      # @param line [String]
+      # @return [Bool]
       def is_record_header?(line)
         segment_peek(line) == @segment_keys.first
       end
 
+      # @param record_file [String] really a whole file in a string? would expect something of IO
+      # @return [Array<Hash{String => String}>]
       def parse_and_zip(record_file)
         record_rows = file_parse(record_file)
         zipped = []
@@ -80,7 +90,6 @@ module Grnds
         end
         zipped
       end
-
     end
   end
 end
--- lib/grnds/ediot/segment_parser.rb
+++ lib/grnds/ediot/segment_parser.rb
@@ -1,9 +1,11 @@
 module Grnds
   module Ediot
     module SegmentParser
-
       SEGMENT_MATCHER = /\*/
 
+      # @param raw_row [String]
+      # @param size [Integer]
+      # @raise Error::SegmentParsingError
       def segment_parse(raw_row:, size:)
         elements = segment_split(raw_row)
         split_size = elements.size - 1 # we don't count the row key in our size. Split size will always be bigger
@@ -11,15 +13,17 @@ module Grnds
         (0..size).map{|x| elements[x] || ''} # Pad out the array with empty strings
       end
 
+      # @param raw_row [String]
       def segment_peek(raw_row)
         segment_split(raw_row).first
       end
 
+      # @param raw_row [String]
       def segment_split(raw_row)
         raw_row.split(SEGMENT_MATCHER, -1).map{|s| s.strip }
       end
 
-      def raise_segment_error(size, split_size, element)
+      private def raise_segment_error(size, split_size, element)
         msg = "Too many elements! Expecting #{size} elements for segment #{element}. Got #{split_size}."
         raise Error::SegmentParsingError.new(msg)
       end
